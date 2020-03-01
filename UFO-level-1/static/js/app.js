17574dded4c6ce72ce3c34018cc265ef4df5d2c9
Michael Hargroder <m.hargroder@outlook.com>
2020-03-01 00:24:08 -0500

    intial progress

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,50 @@
+// from data.js
+var tableData = data;
+var tbody = d3.select('tbody')
+
+// CInitial Load of Table
+
+/*data.forEach(function(sighting){
+
+	var row = tbody.append('tr');
+
+		Object.entries(sighting).forEach(function([key,value]) {
+			var cell= tbody.append('td');
+			cell.text(value);
+
+		});
+	});*/
+
+
+ // Filter User Action
+
+ var inputElement =  d3.select('#datetime');
+ var filterButton = d3.select('#filter-btn');
+
+ inputElement.on('submit', function(event){
+ 	event.preventDefault();
+  });
+
+
+ filterButton.on('click', function(){
+
+  var inputValue = inputElement.property('value');
+
+  console.log(inputValue);
+
+  var filteredSightings = tableData.filter( sighting => sighting.datetime === inputValue)
+
+filteredSightings.forEach(function(sighting){
+
+	var row = tbody.append('tr');
+
+		Object.entries(sighting).forEach(function([key,value]) {
+			var cell= tbody.append('td');
+			cell.text(value);
+
+		});
+	});
+
+ });
+
+
